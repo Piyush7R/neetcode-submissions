64e8e2901e34bc93e79d3e55713341246ece13ef
Piyush Raj <142214168+Piyush7R@users.noreply.github.com>
2026-06-17 10:18:03 +0530

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        map<int, int> m;
+        for(auto s: s1){
+            m[s]++;
+        }
+        
+        for(int i=0; i<s2.size()-s1.size()+1; i++){
+            bool flag = true;
+            map<int, int> m_new = m;
+            int k = i;
+            int j = s1.size();
+            while(k < i+j){
+                if(m_new.find(s2[k]) != m_new.end()){
+                    m_new[s2[k]]--;
+                }
+                k++;
+            }
+            for(auto pair: m_new){
+                if(pair.second != 0){
+                    flag = false;;
+                }
+            }
+            if(flag){
+                return true;
+            }
+        }
+        return false;
+    }
+};
