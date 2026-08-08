5c6f209193ce704ccd133ba72c72cb28a3d5495d
Piyush Raj <142214168+Piyush7R@users.noreply.github.com>
2026-08-09 01:36:13 +0530

    Add: permutation-string - submission-7

---
+++ Data Structures & Algorithms/permutation-string/submission-7.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.size() > s2.size()) return false;
+        vector<int> freq1(26, 0);
+        vector<int> freq2(26, 0);
+
+        for(int i=0; i<s1.size(); i++){
+            freq1[s1[i] - 'a']++;
+        }
+        int left = 0;
+        for(int right=0; right<s2.size(); right++){
+            freq2[s2[right] - 'a']++;
+            if(right - left + 1 > s1.size()){
+                freq2[s2[left] - 'a']--;
+                left++;
+            }
+            if(right-left+1==s1.size()){
+                if(freq1==freq2)
+                    return true;
+            }
+            
+        }
+        return false;
+    }
+};
